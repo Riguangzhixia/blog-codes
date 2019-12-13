6d3384e6286de5708a4a8b093c0925fa85e62e22
彭飞 <pengfei@kingsoft.com>
2019-12-13 18:57:02 +0800

    整理

--- js-native/数组flat.js
+++ js-native/数组flat.js
@@ -1,18 +1,34 @@
 //输入[1,[2,[3,4]]]，输出[1,2,3,4]
-function flat(arr){
-    //1. tostring/join
-    console.log(arr.toString().split(',').map(item => Number(item)));
-    //2. reduce/扩展运算符
+const testarr = [1, [2, [3, 4]]];
+
+function flat(arr) {
+    //1. reduce/扩展运算符
     console.log(
-        arr.reduce((total,item) => {
+        arr.reduce((total, item) => {
             let result = item;
-            if(Array.isArray(result)){
+            if (Array.isArray(result)) {
                 while (result.some(child => Array.isArray(child))) {
                     result = [].concat(...result);
                 }
             }
             return total.concat(result);
-        },[])
+        }, [])
     )
 }
-flat([1,[2,[3,4]]]);
+
+flat(testarr);
+//2. tostring/join
+function flat2(arr) {
+    console.log(arr.toString().split(',').map(item => Number(item)));
+}
+
+flat2(testarr);
+//3. 扩展运算符
+function flattern(arr) {
+    while (arr.some(item => Array.isArray(item))) {
+        arr = [].concat(...arr);
+    }
+    console.log(arr);
+}
+
+flattern(testarr);
